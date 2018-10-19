a9b5bc6eb2ec0ec27029be6c283d50879eeb5160
Jiovana <gomesjiovana@gmail.com>
2018-10-18 22:24:50 -0300

    contador 99 funcionando sem timer

--- cont99v1/cont99v1.ino
+++ cont99v1/cont99v1.ino
@@ -1,5 +1,5 @@
                                 // A B C D E F G   
-byte matriz1[10][7] =            { { 1,1,1,1,1,1,0 },  // = Digito 0
+byte matrizDireita[10][7] =      { { 1,1,1,1,1,1,0 },  // = Digito 0
                                  { 0,1,1,0,0,0,0 },  // = Digito 1
                                  { 1,1,0,1,1,0,1 },  // = Digito 2
                                  { 1,1,1,1,0,0,1 },  // = Digito 3
@@ -11,7 +11,7 @@ byte matriz1[10][7] =            { { 1,1,1,1,1,1,0 },  // = Digito 0
                                  { 1,1,1,0,0,1,1 },  // = Digito 9
                                  };
                                 // A B C D E F G   
-byte matriz2[10][7] =            { { 1,1,1,1,1,1,0 },  // = Digito 0
+byte matrizEsquerda[10][7] =     { { 1,1,1,1,1,1,0 },  // = Digito 0
                                  { 0,1,1,0,0,0,0 },  // = Digito 1
                                  { 1,1,0,1,1,0,1 },  // = Digito 2
                                  { 1,1,1,1,0,0,1 },  // = Digito 3
@@ -22,62 +22,72 @@ byte matriz2[10][7] =            { { 1,1,1,1,1,1,0 },  // = Digito 0
                                  { 1,1,1,1,1,1,1 },  // = Digito 8
                                  { 1,1,1,0,0,1,1 },  // = Digito 9
                                  };                                 
-byte count1 = 0;
-byte count2 = 0;
+
+byte countDireita = 0;
+byte countEsquerda = 0;
+
+/*
+--Utilize o timer 1 para controlar a periodicidade de atualização do
+display 7 segmentos (valor que será apresentado). EU SOU O CONTADOR 
+--Utilize o timer 2 para controlar a periodicidade que o LED pisca
+(apresentação da informação no display). EU SOU O DELAY
+*/
 
 void setup() 
 {
-  pinMode(3, OUTPUT);// matriz direita
-  pinMode(4, OUTPUT);// matriz direita
-  pinMode(5, OUTPUT);// matriz direita
-  pinMode(6, OUTPUT);// matriz direita
-  pinMode(7, OUTPUT);// matriz direita
-  pinMode(8, OUTPUT);// matriz direita
-  pinMode(9, OUTPUT);// matriz direita
-  pinMode(10, OUTPUT);// matriz direita
-  pinMode(30, OUTPUT);// matriz esquerda
-  pinMode(32, OUTPUT);// matriz esquerda
-  pinMode(34, OUTPUT);// matriz esquerda
-  pinMode(36, OUTPUT);// matriz esquerda
-  pinMode(38, OUTPUT);// matriz esquerda
-  pinMode(40, OUTPUT);// matriz esquerda
-  pinMode(42, OUTPUT);// matriz esquerda
-  pinMode(44, OUTPUT);// matriz esquerda
-  pinMode(13, OUTPUT); //led da placa
-  writePonto(0);  //inicializa ponto decimal como desligado
-  pinMode(2, INPUT);//Esta inicializando o botao que esta na posicao 20
- attachInterrupt(0, reset, RISING);//digitalPinToInterrupt(20)
+  byte i;
+  for(i = 0; i<14;++i)
+  {
+    pinMode(i, OUTPUT);//SETANDO LEDS DO DISPLAY DE 7 SEGMENTOS 
+  }  
 }
   
-void writePonto(byte modo)   //Funcao que aciona o ponto no display
-{  
-  digitalWrite(10, modo);
+
+void escreveDisplayDireita(byte numero)  //Funcao que aciona o display
+{
+  byte pino = 0;
+  //Percorre o array ligando os segmentos correspondentes ao digito
+  //for(byte letra = 0; letra <7; ++letra)
+  //{
+    for (byte letra = 0; letra < 7; ++letra)  
+    { 
+    digitalWrite(pino, matrizDireita[numero][letra]);
+    ++pino;
+    }
+  //}
 }
 
-void sevenSegWrite(byte digit)  //Funcao que aciona o display
+void escreveDisplayEsquerda(byte numero)  //Funcao que aciona o display
 {
-  byte pin = 3;
-
+  byte pino2 = 7;
   //Percorre o array ligando os segmentos correspondentes ao digito
-  for (byte segCount = 0; segCount < 7; ++segCount)  
-  { 
-    digitalWrite(pin, seven_seg_digits[digit][segCount]);
-    ++pin;
-  }
-    writePonto(1);  //Liga o ponto
-    delay(100);   //Aguarda 100 milisegundos
-    writePonto(0);  //Desliga o ponto
+    for (byte letra = 0; letra < 7; ++letra)  
+    { 
+    digitalWrite(pino2, matrizEsquerda[numero][letra]);
+    ++pino2;
+    }
 }
 
 void loop() 
 {
-  for(count = 0;count<9)
-  if(count >9)
-   {
-     count = 0;
-   }
-     sevenSegWrite(count);
-     count++;
-    delay(500); 
-}     
-
+    if(countDireita == 9)//quando termina unidade
+    {
+       countEsquerda++;
+      escreveDisplayEsquerda(countEsquerda);
+      countDireita = 0;
+      escreveDisplayDireita(countDireita);
+      delay(500);
+}else
+    { 
+      countDireita++;
+      escreveDisplayDireita(countDireita);
+      delay(500);
+    }
+    if(countDireita == 9 && countEsquerda == 9){
+        countDireita  = 0;
+        countEsquerda = 0;
+      escreveDisplayEsquerda(countEsquerda);
+      escreveDisplayDireita(countDireita);
+        delay(500);
+  }
+}
